7addabf1e06c681593a0026bdf2646a3d965b6b7
Jordan <Jordantay9014@outlook.com>
2022-12-06 22:56:26 -0600

    init commit

---
+++ unique.lib/Unique.cs
@@ -0,0 +1,18 @@
+﻿namespace unique.lib;
+
+public class Unique
+{
+
+    private static Random random = new Random();
+
+    /// <summary>
+    ///  Initializes a new instance of UUID.
+    /// </summary>
+    /// <param name="maxNum"></param>
+    /// <returns>Returns a int UUID</returns>
+    public static string GetId()
+    {
+        var uuid = random.Next() + "-" + random.Next();
+        return uuid;
+    }
+}
